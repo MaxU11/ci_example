0c767d6fd45d463fd742ecac89699ce1af795505
MaxU11 <49079692+MaxU11@users.noreply.github.com>
2020-12-08 21:48:44 +0100

    Add files via upload

--- main_test.go
+++ main_test.go
@@ -2,8 +2,18 @@ package main
 
 import "testing"
 
-func TestSomethingPlease(t *testing.T) {
-	if 1 == 0 {
-		t.Fatal("oh no")
+func TestGetMinuteUp(t *testing.T) {
+
+	res := getMinute(1, 30)
+	if res != 2 {
+		t.Errorf("getMinute(1, 30) = %d; wanted 2", res)
+	}
+}
+
+func TestGetMinuteDown(t *testing.T) {
+
+	res := getMinute(1, 15)
+	if res != 1 {
+		t.Errorf("getMinute(1, 15) = %d; wanted 1", res)
 	}
 }
